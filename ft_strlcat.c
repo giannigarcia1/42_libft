615b8d04e62e5415414eb0a1309976490afd1331
Gianni Garcia <gigarcia@c1r13s2.42madrid.com>
2026-02-13 23:29:21 +0100

    Fixing norminette errors and deleting most mains

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gigarcia <gigarcia@student.42madrid.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 20:57:42 by gigarcia          #+#    #+#             */
-/*   Updated: 2026/01/24 20:32:59 by gigarcia         ###   ########.fr       */
+/*   Updated: 2026/02/13 23:24:09 by gigarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,19 +31,3 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	dest[dest_len + i] = '\0';
 	return (dest_len);
 }
-/*
-#include <bsd/string.h>
-
-int main()
-{
-	char	*src = "hola que tal";
-	char	dest[15] = "12345678910";
-	char	*src2 = "hola que tal";
-	char	dest2[15] = "12345678910";
-
-	size_t	ret = ft_strlcat(dest, src, 6);
-	size_t	ret2 = strlcat(dest2, src2, 6);
-
-	printf("%zu\n dest: %s\n", ret, dest);
-	printf("%zu\n dest2: %s\n", ret2, dest2);
-}*/
